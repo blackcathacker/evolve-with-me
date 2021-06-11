1abc5b7c2e816763acabf4271efc7fe22c802f7f
blackcathacker <douglas.pace@gmail.com>
2021-06-10 18:32:13 -0700

    getNeighbors

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 
 
+
 const evolve = (current) => current
 
+const NEIGHBOR_DIFF = [-1, 0, 1]
+const getNeighbors = (grid, _x, _y) => {
+    return [].concat.apply([], (NEIGHBOR_DIFF.map(i => {
+        y = _y + i
+        if (y >= 0 && y < grid.length) {
+            const innerArray = grid[y]
+            return NEIGHBOR_DIFF.map(j => {
+                x = _x + j
+                if (x >= 0 && x < innerArray.length && !(i === 0 && j === 0)) {
+                    return innerArray[x]
+                }
+            }).filter(_ => _ !== undefined)
+        }
+    }).filter(_ => _ !== undefined)))
+}
+
 module.exports = {
-    evolve
+    evolve,
+    getNeighbors
 }
