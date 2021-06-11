0d7e5de00b56dd26508b70534a287d7ec7b1efb9
blackcathacker <douglas.pace@gmail.com>
2021-06-10 19:02:40 -0700

    test cases pass

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 
+const evolve = (grid, evolutions = 1) => {
+    let current = grid
+    for (let i = 0; i < evolutions; i++) {
+        current = evolveInner(current)
+    }
+    return current
+}
 
-
-const evolve = (current) => {
+const evolveInner = (current) => {
     return current.map((innerArray, y) => {
         return innerArray.map((cell, x) => {
             const neighbors = getNeighbors(current, x, y)
@@ -25,9 +31,18 @@ const evolve = (current) => {
     })
 }
 
+const concatArrays = (...args) => [].concat.apply([], ...args)
+const filterEmpty = _ => _
 const NEIGHBOR_DIFF = [-1, 0, 1]
 const getNeighbors = (grid, _x, _y) => {
-    return [].concat.apply([], (NEIGHBOR_DIFF.map(i => {
+    //loop over the coordinate differences to find all neighbors (-1, 0, 1)
+    //add those values to the current y axis and determine if it is a valid coordinate
+    //if it is do this again but on the x axis checking similarly but also comparing to see
+    //if the difference of x AND y are 0 (in which case its the current cell and not a neighbor)
+    //if all those conditions match return that value.
+    //Concat all arrays together to represent a single list of neighbors and 
+    //then filter out 0 and undefined values which happen when the coordinates are invalid
+    return concatArrays(NEIGHBOR_DIFF.map(i => {
         y = _y + i
         if (y >= 0 && y < grid.length) {
             const innerArray = grid[y]
@@ -36,9 +51,9 @@ const getNeighbors = (grid, _x, _y) => {
                 if (x >= 0 && x < innerArray.length && !(i === 0 && j === 0)) {
                     return innerArray[x]
                 }
-            }).filter(_ => _)
+            })
         }
-    }).filter(_ => _)))
+    })).filter(filterEmpty)
 }
 
 module.exports = {
